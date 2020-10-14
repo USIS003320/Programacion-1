76fae76482c37d55d0d30e969b934b5ed49eae70
famil <famil@DESKTOP-R56DMTU>
2020-10-14 17:51:57 -0500

    Se hicieron cambios en el formulario empleados

--- Calculadora/db_conexion.vb
+++ Calculadora/db_conexion.vb
@@ -19,14 +19,36 @@ Public Class db_conexion
         ds.Clear()
 
         miCommand.Connection = miConexion
-        miCommand.CommandText = "select * from empleados"
 
+        miCommand.CommandText = "select * from empleados"
         miAdapter.SelectCommand = miCommand
         miAdapter.Fill(ds, "empleados")
 
         Return ds
     End Function
+    Public Function mantenimientoDatosEmpleado(ByVal datos As String(), ByVal accion As String)
+        Dim sql, msg As String
+        Select Case accion
+            Case "Nuevo"
+                sql = "INSERT INTO empleados (codigo,nombre,direccion,telefono) VALUES('" + datos(1) + "','" + datos(2) + "','" + datos(3) + "','" + datos(4) + "')"
+            Case "modificar"
+                sql = "UPDATE empleados SET codigo='" + datos(1) + "',nombre='" + datos(2) + "',direccion='" + datos(3) + "',telefono='" + datos(4) + "' WHERE idEmpleado='" + datos(0) + "'"
+            Case "eliminar"
+                sql = "DELETE FROM empleados WHERE idEmpleado='" + datos(0) + "'"
+        End Select
+        If (executesql(sql) > 0) Then
+            msg = "Accion terminada con exito"
+        Else
+            msg = "Fallo el proceso"
+        End If
+        Return msg
+    End Function
 
+    Private Function executesql(ByVal sql As String)
+        miCommand.Connection = miConexion
+        miCommand.CommandText = sql
+        Return miCommand.ExecuteNonQuery()
+    End Function
 
 
 
--- Calculadora/frmEmpleados.vb
+++ Calculadora/frmEmpleados.vb
@@ -2,6 +2,7 @@
     Dim objConexion As New db_conexion
     Dim dataTable As New DataTable
     Dim posicion As Integer
+    Dim accion As String = "Nuevo"
 
     Private Sub frmEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         posicion = 0
@@ -15,12 +16,19 @@
     End Sub
 
     Sub mostrarDatos()
-        txtCodigoEmpleado.Text = dataTable.Rows(posicion).ItemArray(1).ToString()
-        txtNombreEmpleado.Text = dataTable.Rows(posicion).ItemArray(2).ToString()
-        txtDireccionEmpleado.Text = dataTable.Rows(posicion).ItemArray(3).ToString()
-        txtTelefonoEmpleado.Text = dataTable.Rows(posicion).ItemArray(4).ToString()
+        If dataTable.Rows.Count > 0 Then
 
-        lblRegistroEmpleado.Text = posicion + 1 & " de " & dataTable.Rows.Count
+            Me.Tag = dataTable.Rows(posicion).ItemArray(0).ToString()
+            txtCodigoEmpleado.Text = dataTable.Rows(posicion).ItemArray(1).ToString()
+            txtNombreEmpleado.Text = dataTable.Rows(posicion).ItemArray(2).ToString()
+            txtDireccionEmpleado.Text = dataTable.Rows(posicion).ItemArray(3).ToString()
+            txtTelefonoEmpleado.Text = dataTable.Rows(posicion).ItemArray(4).ToString()
+
+            lblRegistroEmpleado.Text = posicion + 1 & " de " & dataTable.Rows.Count
+        Else
+            limpiarDatosEmpleado()
+            MessageBox.Show("No hay registrp que mostrar", "Registro de empleados", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
     End Sub
 
     Private Sub btnPrimeroEmpleado_Click(sender As Object, e As EventArgs) Handles btnPrimeroEmpleado.Click
@@ -55,33 +63,59 @@
         If btnAgregarEmpleado.Text = "Nuevo" Then
             btnAgregarEmpleado.Text = "Guardar"
             btnModificarEmpleado.Text = "Cancelar"
+            accion = "Nuevo"
 
             HabDescontroles(False)
+            limpiarDatosEmpleado()
         Else
+            Dim msg = objConexion.mantenimientoDatosEmpleado(New String() {
+                Me.Tag, txtCodigoEmpleado.Text, txtNombreEmpleado.Text, txtDireccionEmpleado.Text, txtTelefonoEmpleado.Text
+            }, accion)
+            MessageBox.Show(msg, "Registro de clientes", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
+            obtenerDatos()
             HabDescontroles(True)
             btnAgregarEmpleado.Text = "Nuevo"
             btnModificarEmpleado.Text = "Modificar"
         End If
     End Sub
 
-    Sub HabDescontroles(ByVal estado As Boolean)
-        grbDatos.Enabled = estado
+    Private Sub HabDescontroles(ByVal estado As Boolean)
+        grbDatos.Enabled = Not estado
         grbNavegacion.Enabled = estado
         btnEliminarEmpleado.Enabled = estado
         btnBuscarEmpleado.Enabled = estado
+    End Sub
+
+    Private Sub limpiarDatosEmpleado()
+        txtCodigoEmpleado.Text = ""
+        txtNombreEmpleado.Text = ""
+        txtDireccionEmpleado.Text = ""
+        txtTelefonoEmpleado.Text = ""
     End Sub
 
     Private Sub btnModificarEmpleado_Click(sender As Object, e As EventArgs) Handles btnModificarEmpleado.Click
         If btnModificarEmpleado.Text = "Modificar" Then
             btnAgregarEmpleado.Text = "Guardar"
             btnModificarEmpleado.Text = "Cancelar"
-
+            accion = "Modificar"
             HabDescontroles(False)
         Else
-
+            obtenerDatos()
+            HabDescontroles(True)
             btnAgregarEmpleado.Text = "Nuevo"
             btnModificarEmpleado.Text = "Modificar"
+        End If
+    End Sub
+
+    Private Sub btnEliminarEmpleado_Click(sender As Object, e As EventArgs) Handles btnEliminarEmpleado.Click
+        If (MessageBox.Show("Esta seguro de borrar a " + txtNombreEmpleado.Text, "Registro de empleado",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes) Then
+            objConexion.mantenimientoDatosEmpleado(New String() {Me.Tag}, "eliminar")
+            If posicion > 0 Then
+                posicion -= 1
+            End If
+            obtenerDatos()
         End If
     End Sub
 End Class
