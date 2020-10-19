4854f4ae3a4333abc7b9509fdb2a2715264f83c5
Mauricio Adonay Espinoza Villatoro <usis018120@ugb.edu.sv>
2020-10-19 00:06:55 -0600

    Formulario de Producto

--- Calculadora/My Project/Application.Designer.vb
+++ Calculadora/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Calculadora.frmEmpleados
+            Me.MainForm = Global.Calculadora.frmProducto
         End Sub
     End Class
 End Namespace
